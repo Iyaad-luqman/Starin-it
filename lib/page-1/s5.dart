998b58c03782655e2b44d414369b2f259c7f87db
Mudhassir <mudhassirhabeeb74@gmail.com>
2024-01-11 23:03:26 +0530

    gender connection done

--- lib/page-1/s5.dart
+++ lib/page-1/s5.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:starinit/page-1/s5a.dart';
 import 'package:starinit/page-1/s6.dart';
 import 'package:starinit/utils.dart';
 
@@ -629,7 +630,7 @@ class S5 extends StatelessWidget {
                           onPressed: () {
                             Navigator.push(
                               context,
-                              MaterialPageRoute(builder: (context) => S6()),
+                              MaterialPageRoute(builder: (context) => S5a()),
                             );
                           },
                           style: TextButton.styleFrom(
