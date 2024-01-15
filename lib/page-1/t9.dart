7ea962b58c041619e6dde2e4d0cff9c60f7dec5c
Mudhassir <mudhassirhabeeb74@gmail.com>
2024-01-15 13:54:20 +0530

    added t series

---
+++ lib/page-1/t9.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:starinit/utils.dart';
+
+class T9 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double baseWidth = 360;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
+    double ffem = fem * 0.97;
+    return Container(
+      width: double.infinity,
+      child: Container(
+        // t9Nzp (30:115)
+        padding: EdgeInsets.fromLTRB(29 * fem, 26 * fem, 31 * fem, 115 * fem),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Color(0xffffffff),
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(
+              'assets/page-1/images/plane-bg-13-bg-GxC.png',
+            ),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              // autogroupkjb6eBe (JHy9baHqCsLYDNppxpKJb6)
+              margin:
+                  EdgeInsets.fromLTRB(12 * fem, 0 * fem, 0 * fem, 103 * fem),
+              width: 288 * fem,
+              height: 150 * fem,
+              child: Stack(
+                children: [
+                  Positioned(
+                    // secondsa5J (30:123)
+                    left: 29.5 * fem,
+                    top: 0 * fem,
+                    child: Align(
+                      child: SizedBox(
+                        width: 220 * fem,
+                        height: 150 * fem,
+                        child: Text(
+                          '180\nSECONDS',
+                          textAlign: TextAlign.center,
+                          style: SafeGoogleFont(
+                            'Vazirmatn',
+                            fontSize: 48 * ffem,
+                            fontWeight: FontWeight.w400,
+                            height: 1.5625 * ffem / fem,
+                            letterSpacing: 0.72 * fem,
+                            color: Color(0xffffffff),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    // rYc (30:124)
+                    left: 0 * fem,
+                    top: 0 * fem,
+                    child: Container(
+                      width: 288 * fem,
+                      height: 150 * fem,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15 * fem),
+                      ),
+                      child: Center(
+                        // rectangle13CMa (30:125)
+                        child: SizedBox(
+                          width: double.infinity,
+                          height: 150 * fem,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15 * fem),
+                              border: Border.all(color: Color(0x7fffffff)),
+                              color: Color(0x7f19173d),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              // LCt (30:117)
+              margin: EdgeInsets.fromLTRB(2 * fem, 0 * fem, 0 * fem, 38 * fem),
+              width: 298 * fem,
+              height: 321 * fem,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15 * fem),
+                border: Border.all(color: Color(0x7fffffff)),
+                color: Color(0x7f19173d),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  errorBorder: InputBorder.none,
+                  disabledBorder: InputBorder.none,
+                  contentPadding: EdgeInsets.fromLTRB(
+                      16 * fem, 13 * fem, 16 * fem, 13 * fem),
+                  hintText: 'Give your answer...',
+                  hintStyle: TextStyle(color: Color(0xffffffff)),
+                ),
+                style: SafeGoogleFont(
+                  'Urbanist',
+                  fontSize: 12 * ffem,
+                  fontWeight: FontWeight.w200,
+                  height: 1.2 * ffem / fem,
+                  letterSpacing: 0.18 * fem,
+                  color: Color(0xff000000),
+                ),
+              ),
+            ),
+            TextButton(
+              // savejkp (30:119)
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.zero,
+              ),
+              child: Container(
+                width: double.infinity,
+                height: 47 * fem,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Color(0x7fffffff)),
+                  borderRadius: BorderRadius.circular(15 * fem),
+                  gradient: LinearGradient(
+                    begin: Alignment(-0.654, -1),
+                    end: Alignment(0.764, 1),
+                    colors: <Color>[Color(0xff89b4bb), Color(0xff2c59ef)],
+                    stops: <double>[0, 1],
+                  ),
+                ),
+                child: Center(
+                  child: Text(
+                    'CONTINUE',
+                    textAlign: TextAlign.center,
+                    style: SafeGoogleFont(
+                      'Urbanist',
+                      fontSize: 24 * ffem,
+                      fontWeight: FontWeight.w800,
+                      height: 1.2 * ffem / fem,
+                      letterSpacing: 0.36 * fem,
+                      color: Color(0xffffffff),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
